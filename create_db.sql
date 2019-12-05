1c44e549b2baf7079643c1fe5d53ad0e36924623
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-12-05 12:18:57 +1100

    initial commit

---
+++ create_db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    author VARCHAR (100)
+);
+
+INSERT INTO book (title, author)
+    VALUES ('Fablehaven', 'Brandon Mull');
+
+INSERT INTO book (title, author)
+    VALUES ('The Selection', 'Kiera Cass');
+
+INSERT INTO book (title, author)
+    VALUES ('Harry Potter and the Sorcerer&#039;s Stone', 'J. K. Rowling');
+
+CREATE USER bookcataloguser WITH PASSWORD 'read';
+GRANT SELECT, INSERT, UPDATE ON book TO bookcataloguser;
+GRANT USAGE, SELECT ON SEQUENCE book_id_seq TO bookcataloguser;
+    
---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require('body-parser');
+
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL || "postgres://bookcataloguser:read@localhost:5432/bookcatalog";
+const pool = new Pool({connectionString: connectionString});
+
+const myapikey = "20a6c88e296ccb9a84900de16bdc0d715e85d073";
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.urlencoded({ 
+    extended: true
+}));
+  
+app.use(bodyParser.json());
+
+app.set("port", (process.env.PORT || 5000));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', showForm);
+
+app.post('/getBook', getBook);
+
+app.listen(app.get("port"), function() {
+    console.log("Listening on port: ", app.get("port"));
+});
+
+function showForm(req, res) {
+    console.log("showing form");
+    //res.send('hello');
+    res.render('searchform', {title:"", author:""});
+}
+
+function getBook(req, res) {
+    console.log("Getting book info");
+
+    var searchText = req.body.searchText
+
+    getBookFromDatabase(searchText, function(error, result) {
+        console.log("Here's the result ", result);
+        console.log(result.length);
+
+        for (i=0; i < result.length; i++) {
+            Object.entries(result[i]).forEach(
+                ([key, value]) => console.log(key, value)
+            );
+            
+            var title = result[i].title;
+            console.log(title);
+
+            var author = result[i].author;
+            console.log(author);
+            var params = {searchText: searchText, title: title, author: " by: " + author}
+
+            res.render('searchform', params);
+        }
+    })
+}
+
+function getBookFromDatabase (searchText, callback) {
+    console.log("get book with title " + searchText);
+    const sql = "SELECT id, title, author FROM book WHERE title = $1::text";
+    params = [searchText];
+
+    pool.query(sql, params, function(err, result) {
+        if (err) {
+            console.log("error in search");
+            callback(err, null);
+        }
+        console.log("here's the book " + JSON.stringify(result.rows));
+        callback(null, result.rows);
+    })
+}
